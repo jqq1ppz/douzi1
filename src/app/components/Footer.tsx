65ccc72df65010aa1de3a8ab718919519d3e67c0
jqq1ppz <jqq1ppz@gmail.com>
2026-04-26 15:29:22 -0700

    initial commit

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
 	return (
 		<footer className=" bottom-0 w-full py-2 text-center text-gray-400 text-sm bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 border-t border-purple-500/20">
-			<p>© {new Date().getFullYear()}. Hosted by dandan</p>
+			<p>© {new Date().getFullYear()}. Hosted by jiahe</p>
 		</footer>
 	);
 }
